7d80cc7eea44884e2b909103e48fab9f43fa4b19
jOSEsOUSAFh <89113025+jOSEsOUSAFh@users.noreply.github.com>
2022-07-10 16:13:26 -0300

    Update indexadm.php

--- indexadm.php
+++ indexadm.php
@@ -242,9 +242,10 @@ while($retorno = mysqli_fetch_object($execute)){ ?>
         
 <?php 
 
-include('includes/script.php');
+
 include('includes/modal.php');
 include('includes/footer.php');
+include('includes/script.php');
 
 
 ?>
