6af3ee6d1973f1792429d20105ba498febfc4b6e
jOSEsOUSAFh <89113025+jOSEsOUSAFh@users.noreply.github.com>
2022-07-10 13:46:13 -0300

    att

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ if(isset($_SESSION["usuario"]) && is_array($_SESSION["usuario"])){
 if($adm == 1){
 
     
-    include('indexadm.php');
+    require('indexadm.php');
 
 
    }elseif($adm == 0){
        
        
-     include('indexuser.php');
+     require('indexuser.php');
 
 
    }else{
