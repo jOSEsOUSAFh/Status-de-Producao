8f8951597e59341d01cb009fa0eb146f3efa5ccf
jOSEsOUSAFh <89113025+jOSEsOUSAFh@users.noreply.github.com>
2022-07-14 22:06:50 -0300

    fav-icon
    atualizar o cdn dos icons

--- includes/header.php
+++ includes/header.php
@@ -12,14 +12,14 @@
     <title>Projeto</title>
 
         <!-- Custom fonts for this template-->
-        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <!-- <link href="css/alla.css" rel="stylesheet"> -->
+    
+    <script src="https://kit.fontawesome.com/31a35138c2.js" crossorigin="anonymous"></script>
 
  
     
